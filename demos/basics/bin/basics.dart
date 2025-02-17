b345bd2892592918142c3d3e90f8171a39834f42
Joey Boerwinkel <joey@iconica.nl>
2025-02-17 13:21:14 +0100

    add dart program and hot reload demo

---
+++ demos/basics/bin/basics.dart
@@ -0,0 +1,26 @@
+main() {
+  String name = "Joey";
+  String hello = "Hello $name";
+
+  int age = 30;
+
+  List<String> participants = ["Nick", "Thomas", "Fleur", "Martijn", "Lasse"];
+
+  Map<String, Object?> grades = {
+    "Pietje": 10,
+  };
+
+  int? optionalValue = null;
+
+  var record = ('first', a: 2, b: true, 'last');
+
+  optionalValue ??= 1;
+  optionalValue ??= 2;
+
+  print(optionalValue);
+
+  print(participants[2]);
+  print(hello);
+  print(grades["Pietje"]);
+  print(record.b);
+}
